545a4ee934e9973ad2ca546535ce0c3f6f6f475d
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2024-11-03 20:13:21 +0000

    Modified functions of each transaction

--- control_flow_atm.py
+++ control_flow_atm.py
@@ -1,143 +1,42 @@
-#Prompt for User to confirm if he/she has an account
+depositors_balance = 0
 
-print('Welcome to RholAnt Bank ATM Services.')
-prompt=('Do you have an account with us?\n1. Yes\n2. No\nEnter number associated to your choice: ')
-
-
-user_choice = int(input(prompt))
-
-depositors_balance = 0 
-
-def create_account():
-    global depositors_balance
-    customer_name = input('\nEnter Full Name: ') 
-    customer_pin = int(input('Set a 4 digit Pin: '))
-    customer_email = input('Your Email: ')
-    credentials = {
-        'name':customer_name,
-        'pin': customer_pin,
-        'email': customer_email
-    }
-    print('Account created successfully!!\n')
-  
-  
-
-
-    
-def new_user_deposit():
-    global depositors_balance
-    print(f'Your account is {depositors_balance}$ so deposit to continue')
-    balance = (int(input('How much would you like to deposit?: ')))
-    depositors_balance += balance
-    print(f'Successfully deposited {balance}')
+#Function to check Balance
    
-   
-#Function to check Balance   
 def check_balance():
-     global depositors_balance
-     #Call verify pin function
-     print(f'Current Balance:{depositors_balance}') 
-     main_menu()
-     
-
-#Withdraw funds
-withdraw = 0     
-def withdraw_funds():
-    #Pin function before withdrawal
     global depositors_balance
-    withdraw = int(input('Enter withdrawal amount: '))   
+    authenticate_user(user_pin)
+    print(f"Current Balance: ${depositors_balance:.2f}\n")
+
+#Deposit funds function
+def deposit_funds():
+    global depositors_balance
+
+    print(f'Your account is ${depositors_balance:.2f}\n')
+    amount = float(input('Enter an amount to deposit: '))
+    
+    if amount > 0:
+        print('Enter your pin to proceed')
+        authenticate_user(user_pin)
+        depositors_balance += amount
+        print(f'Successfully deposited ${amount:.2f}\nCrrent Balance: ${depositors_balance:.2f}\n')  
+    else:
+        print('Enter a non-negative amount to deposit') 
+
+
+#Withdraw funds function
+withdraw: float    
+def withdraw_funds():
+    global depositors_balance
+    withdraw = float(input('Enter withdrawal amount: '))
     if withdraw > depositors_balance:
         print('Insufficient Balance')
         withdraw_funds()
     elif withdraw <= 0:
-        print('Invalid Amount') 
+        print('Enter a non-negative amout to withdraw') 
         withdraw_funds()  
     else:
+        print('Enter your pin to proceed')
+        authenticate_user(user_pin)
         depositors_balance -= withdraw
-        print('Money withdrawn successfullly')  
-        main_menu()   
-      
+        print(f'An amount of ${withdraw:.2f} withdrawn successfullly\nCurrent balace is: ${depositors_balance:.2f}')  
 
-
-choice = 0   
-           
-#function to display main menu    
-
-            
-
-#Deposit Funds
-def deposit_funds():
-    #Call pin function
-    global depositors_balance
-    amount = int(input('Enter an amount to deposit: '))
-    if amount > 0:
-        depositors_balance += amount
-        print(f'Account updated successfully!! Crrent Balance: {depositors_balance}')  
-    else:
-        print('Amount rejected')  
-    main_menu()       
-       
-
-
-          
-if user_choice == 1:
-    print("Please Login with your pin")
-    main_menu()
-elif user_choice == 2:
-       print('\nThen create an account Create Account?\n1. Yes\n2. No')  
-       answer = int(input('Choose an option: '))
-       if answer == 1:
-          create_account()
-          new_user_deposit()
-          main_menu()
-       else:   
-          print('Thank you for visiting Rholant ATM Services.\nWe hope you come next time.')
-else:
-    print('Invalid input try again')      
-      
-
-
-    
-  
-   
-        
-
-
-# Function to save user pin
-def save_pin():
-    global pin
-    while True:
-        pin = input('Add a 4-digit pin: ')
-
-        if pin.isdigit() and len(pin) == 4:
-            print('Pin saved successfully')
-            break
-        else:
-            print('Please enter a 4-digit pin.')
-            
-save_pin()
-
-
-# User authentication
-user_pin = ''
-def authenticate_user(user_pin):
-    max_attempts = 3
-    attempts = 0
-    
-    while attempts < max_attempts:
-        user_pin = input('Enter your pin: ')
-
-        if user_pin == pin:
-            print('Authentication successful!')
-            print('Main Menu function goes here!')
-
-            break
-        else:
-            attempts += 1
-            print('Incorrect pin. Try again')
-
-        if attempts == max_attempts:
-            print('Too many incorrect attempts. Access blocked.')
-            exit()
-
-authenticate_user(user_pin)
